72f82f2d2eb900cb12ec409761b3601e7703a3f8
ingridankersen <ingrideankersen@gmail.com>
2020-03-24 15:44:26 +0100

    Uploading project

---
+++ main.js
@@ -0,0 +1,8 @@
+function mobileNavigation() {
+  var x = document.getElementById("mobileNavigation");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
